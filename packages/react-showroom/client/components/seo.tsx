fe4e25fdb04015938767a979c5d6d6944786f95b
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-09-15 01:38:12 +0800

    feat: Add basic SEO improvement (#15)
    * add seo improvement
    
    * add og meta tags
    
    * add changelog

--- packages/react-showroom/client/components/seo.tsx
+++ packages/react-showroom/client/components/seo.tsx
@@ -1,10 +1,24 @@
 import { Helmet } from 'react-helmet';
 import { THEME } from '../theme';
 
-export const Seo = (props: { title?: string }) => (
+export const Seo = (props: { title?: string; description?: string }) => (
   <Helmet
     title={props.title}
     defaultTitle={THEME.title}
     titleTemplate={`%s | ${THEME.title}`}
-  />
+  >
+    {props.title && <meta name="twitter:title" content={props.title}></meta>}
+    <meta name="twitter:card" content="summary"></meta>
+    {props.description && (
+      <meta name="description" content={props.description}></meta>
+    )}
+    {props.description && (
+      <meta name="twitter:description" content={props.description}></meta>
+    )}
+    <meta property="og:type" content="article" />
+    {props.title && <meta property="og:title" content={props.title} />}
+    {props.description && (
+      <meta property="og:description" content={props.description} />
+    )}
+  </Helmet>
 );
