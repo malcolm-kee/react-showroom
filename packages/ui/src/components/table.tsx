449b0b340ee1ecd4b9bc2245318da1858c277af1
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-09-19 17:25:28 +0800

    support gfm

---
+++ packages/ui/src/components/table.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { styled } from '../stitches.config';
+
+const StyledTable = styled('table', {
+  minWidth: '100%',
+});
+
+const TableImpl = function Table({
+  children,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'>) {
+  return (
+    <TableWrapper {...props}>
+      <StyledTable>{children}</StyledTable>
+    </TableWrapper>
+  );
+};
+
+const TableWrapper = styled('div', {
+  overflowX: 'auto',
+});
+
+const Tr = styled('tr', {
+  '&:nth-child(2n)': {
+    backgroundColor: '$gray-50',
+  },
+  '&:hover': {
+    backgroundColor: '$gray-100',
+  },
+});
+
+const Th = styled('th', {
+  py: '$2',
+  px: '$4',
+  fontSize: '$sm',
+  lineHeight: '$sm',
+  backgroundColor: '$gray-200',
+  borderBottom: '1px solid $gray-300',
+  textAlign: 'left',
+});
+
+const Td = styled('td', {
+  py: '$1',
+  px: '$4',
+});
+
+export const Table = Object.assign(TableImpl, {
+  Th,
+  Td,
+  Tr,
+});
