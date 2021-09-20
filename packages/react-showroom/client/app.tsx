89806fb706dcb97ce2eef2ca897403f6b5999210
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-09-20 11:04:06 +0800

    fix: broken site

--- packages/react-showroom/client/app.tsx
+++ packages/react-showroom/client/app.tsx
@@ -46,7 +46,11 @@ const routes = sections.map(function SectionRoute(section) {
 
   if (section.type === 'markdown') {
     return (
-      <Route path={`/${section.slug}`} key={section.slug}>
+      <Route
+        path={`/${section.slug}`}
+        exact={section.slug === ''}
+        key={section.slug}
+      >
         <MarkdownRoute section={section} />
       </Route>
     );
