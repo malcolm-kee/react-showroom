9dfc888c9e6dff3c4b4ab5fd80d11e9a8cb06ca2
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-10-23 15:07:30 +0800

    fix test

--- packages/core/src/display-name.spec.ts
+++ packages/core/src/display-name.spec.ts
@@ -1,8 +1,8 @@
 import { decodeDisplayName, encodeDisplayName } from './display-name';
 
 test('encodeDisplayName', () => {
-  expect(encodeDisplayName('TextInput')).toBe('_text_input');
-  expect(encodeDisplayName('HTMLInput')).toBe('_h_t_m_l_input');
+  expect(encodeDisplayName('TextInput')).toBe('-text-input');
+  expect(encodeDisplayName('HTMLInput')).toBe('-h-t-m-l-input');
 });
 
 ['a', 'A', 'TextInput', 'HTMLInput'].forEach((name) => {
