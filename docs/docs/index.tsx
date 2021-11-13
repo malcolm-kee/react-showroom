5758fc9306e79dd449727c623839432854f93dcc
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-11-13 22:35:16 +0800

    update docs (#87)

--- docs/docs/index.tsx
+++ docs/docs/index.tsx
@@ -51,7 +51,6 @@ export const HomePage = () => {
       </div>
       <div className="py-12 bg-white">
         <div className="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="sr-only">A better way to send money.</h2>
           <dl className="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-8">
             {features.map((feature) => (
               <div key={feature.name}>
