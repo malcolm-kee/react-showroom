07ba4b7f89aad3d56b4ea6581da30ef4c9b6cf84
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-06-22 15:37:38 +0800

    improve test

--- examples/react-example/tests/react-example-third-party.spec.ts
+++ examples/react-example/tests/react-example-third-party.spec.ts
@@ -4,8 +4,24 @@ test.describe('react example third party integration', () => {
   test('msw mock should work', async ({ page }) => {
     await page.goto('/core/button');
 
-    await page.click('button:has-text("Fetch Data")');
+    async function clickButtonAndVerifyResult(retryCount = 0) {
+      try {
+        await page.click('button:has-text("Fetch Data")');
 
-    await expect(page.locator('text=Result intercepted by msw!')).toBeVisible();
+        await expect(
+          page.locator('text=Result intercepted by msw!')
+        ).toBeVisible({ timeout: 3000 });
+      } catch (error) {
+        if (retryCount <= 3) {
+          console.log(error);
+          await new Promise((fulfill) => setTimeout(fulfill, 1000));
+          await clickButtonAndVerifyResult(retryCount + 1);
+        } else {
+          throw error;
+        }
+      }
+    }
+
+    await clickButtonAndVerifyResult();
   });
 });
--- examples/subpath-example/tests/subpath.spec.ts
+++ examples/subpath-example/tests/subpath.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test, errors } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.describe('subpath example', () => {
   test('visit home page', async ({ page }) => {
